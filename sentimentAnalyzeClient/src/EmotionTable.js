91d656e8ae79b1ba5b1de2394229ceff0caaf2a5
Jorge Maricato <jorgemaricato@gmail.com>
2021-10-01 00:09:28 -0300

    "Server and client side changes to implement the final project requirements"
    "Server and client side changes to implement the final project requirements"

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -2,8 +2,18 @@ import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
+    getEmotionTable = () => Object.entries(this.props.emotions).map(function(mapentry) {
+        return (
+            <tr>
+            <td>{mapentry[0]}</td>
+            <td>{mapentry[1]}</td>
+            </tr>
+        )
+    });
+
     render() {
       //Returns the emotions as an HTML table
+      let emotionTable = this.getEmotionTable();
       return (  
         <div>
           <table className="table table-bordered">
@@ -12,6 +22,7 @@ class EmotionTable extends React.Component {
               /*Write code to use the .map method that you worked on in the 
               Hands-on React lab to extract the emotions. If you are stuck,
               please click the instructions to see how to implement a map*/
+              emotionTable
             }
             </tbody>
           </table>
